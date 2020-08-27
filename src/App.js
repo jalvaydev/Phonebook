eea5bc685497af9335ff28ac8aac2c02c00f7a05
Jesus Alvarez <jalvaydev@gmail.com>
2020-08-27 11:06:41 -0400

    Retrieve directory from json-server

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Search from './Search'
 import Form from './Form'
 import Directory from './Directory'
+import axios from 'axios'
 
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
   const [ newSearch, setNewSearch] = useState('')
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
 
   const handleNameInput = (event) => {
